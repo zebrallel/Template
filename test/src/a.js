3684f616772ffad758d8ada6c8339bb403a85460
xuejian.xu01 <xuejian.xu01@liulishuo.com>
2020-07-13 19:57:48 +0800

    chore(test): test code

---
+++ test/src/a.js
@@ -0,0 +1,3 @@
+import './a.css'
+
+document.write('this is a.js')
---
+++ test/test.webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    out_a: './src/a.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist_tests'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          // {
+          //   loader: MiniCssExtractPlugin.loader,
+          // },
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('autoprefixer')()
+              ]
+            }
+          }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // new MiniCssExtractPlugin({
+    //   filename: 'css/[name].[contenthash:8].css',
+    //   chunkFilename: 'css/[name].[contenthash:8].chunk.css',
+    // }),
+  ],
+};
